e5fcbdf461e39ed8488ee478b1bdf9b6fcf39e31
Cacivy <609448234@qq.com>
2016-07-09 15:01:49 +0800

    add userscripts
    知乎外链直接跳转

---
+++ configs/userscripts.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         ZhiHuLink
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Cacivy
+// @match        http://www.zhihu.com/*
+// @include      http://www.zhihu.com/*
+// @match        https://www.zhihu.com/*
+// @include      https://www.zhihu.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+        // Your code here...
+        var arr = [];
+        var loading=false;
+        redirect();
+        setInterval( function () {
+        	redirect();
+        } ,3000);
+        function redirect(){
+        	if(!loading){
+        		loading=true;
+        		var links=document.getElementsByTagName('a');
+        		var href;
+        		var length= links.length;
+        		for (var i = 0; i < length; i++) {
+        			if(arr.indexOf(links[i])>-1){
+        				continue;
+        			}
+        			href= links[i].getAttribute('href');
+        			if(href && href.indexOf('//link.zhihu.com')>-1){
+        				links[i].setAttribute('href',unescape(href.split('target=')[1]));
+        			}
+        			arr.push(links[i]);
+        		}
+        		loading=false;
+        	}
+        }
+    })();
