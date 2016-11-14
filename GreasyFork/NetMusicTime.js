6cec1380d6f356f91904e7ed39dfce94b4033f27
Zhan Tao <q609448@live.com>
2016-11-14 15:46:11 +0800

    Update NetMusicTime.js
    支持电台

--- GreasyFork/NetMusicTime.js
+++ GreasyFork/NetMusicTime.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NetMusicTime
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/Cacivy/utils/blob/master/GreasyFork/NetMusicTime.js
+// @version      0.2
 // @description  查看网易云音乐歌单总时间
 // @author       Cacivy
 // @match        http://music.163.com/*
@@ -11,19 +11,14 @@
 (function() {
     'use strict';
 
-    var _m_tables = document.getElementsByClassName('m-table');
-    if (_m_tables) {
-        var _table = _m_tables[0];
-        if (_table && _table.children) {
-            var _tbody = _table.children[1];
-            var _trs = _tbody.children;
+        var _trs = document.querySelectorAll('.m-table tbody tr');
+        if (_trs && _trs.length) {
             var times = [];
-            for (var i = 0; i< _trs.length; i++) {
-                var _tr = _trs[i];
+             Array.prototype.forEach.call(_trs, function(_tr) {
                 var _span = _tr.children[2].children[0];
                 var time = _span.innerText;
                 times.push(time);
-            }
+            })
             if (times.length) {
                 var h=0, m=0, s=0;
                 times.forEach(function(x) {
@@ -45,7 +40,6 @@
             console.log('Time: '+text);
             var _span = document.getElementsByClassName('s-fc4')[0];
             _span.innerText = _span.innerText  + ' ('+text+')';
-        }
     }
 }
 })();
